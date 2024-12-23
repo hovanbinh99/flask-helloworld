76daf7b0990aa3f7c0b3e9ed724342cfdaf1c3fb
hovanbinh123 <ho.van.binh@ar-system.co.jp>
2024-12-23 16:55:17 +0900

    port

--- src/main.py
+++ src/main.py
@@ -13,6 +13,6 @@ class HelloWorld(Resource):
 api.add_resource(HelloWorld, '/')
 
 if __name__ == '__main__':
-  app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
+  app.run(debug=True, host='127.0.0.1', port=int(os.environ.get('PORT', 8080)))
 
 
